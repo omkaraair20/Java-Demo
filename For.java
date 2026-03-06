589a4fc229cb5a760f0ffb9792e2f76360dfa897
Omkar Aair <omkaraair74@gmail.com>
2026-03-06 16:36:00 +0530

    Add files via upload

---
+++ For.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class For {
+	public static void main(String[] args) {
+
+        int arr[] = {10,20,30,40};
+
+        for(int x : arr) {
+            System.out.println(x);
+        }
+    }
+}
+
