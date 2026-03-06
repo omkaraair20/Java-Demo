6656ab9347d40249be2e3719b39e2e0823c452af
Omkar Aair <omkaraair74@gmail.com>
2026-03-06 12:58:40 +0530

    Static & Non-Static

---
+++ Demo.java
@@ -0,0 +1,10 @@
+package Static;
+
+public class Demo {
+	static {
+        System.out.println("Static block executed");
+    }
+	public static void main(String[] args) {
+		System.out.println("Main method");
+    }
+ }
