747ba9ce3c3f72799bcdee4c824f019a3065ec26
Omkar Aair <omkaraair74@gmail.com>
2026-03-06 16:23:41 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+	public static void main(String[] args) {
+
+        int arr[] = new int[5];
+
+        arr[0] = 10;
+        arr[1] = 20;
+
+        System.out.println(arr[0]);
+    }
+}
