5ea79b92f90530415386e10b1f140b242f2399ad
Omkar Aair <omkaraair74@gmail.com>
2026-03-06 17:15:50 +0530

    inheritance

---
+++ Dog1.java
@@ -0,0 +1,14 @@
+package OOP;
+
+class Animal {
+    void eat() {
+        System.out.println("Animal eats");
+    }
+}
+
+class Dog1 extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
