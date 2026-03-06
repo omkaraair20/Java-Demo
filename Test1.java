51235f268bb765feb7ad8111e57eb5b2819e253c
Omkar Aair <omkaraair74@gmail.com>
2026-03-06 13:29:31 +0530

    default value

---
+++ Test1.java
@@ -0,0 +1,11 @@
+public class Test1 {
+	 int a;
+	    boolean b;
+
+	    public static void main(String[] args) {
+	        Test1 t = new Test1();
+	        System.out.println(t.a);
+	        System.out.println(t.b);
+	    }
+}
+
