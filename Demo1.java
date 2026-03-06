fa98d2289a3262685792945446e1f6f78e7efa4b
Omkar Aair <omkaraair74@gmail.com>
2026-03-06 13:24:38 +0530

    Static

---
+++ Demo1.java
@@ -0,0 +1,10 @@
+package Static;
+
+public class Demo1 {
+	static {
+        System.out.println("Static block executed");
+    }
+	public static void main(String[] args) {
+		System.out.println("Main method");
+    }
+ }
